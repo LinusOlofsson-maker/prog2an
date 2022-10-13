e20276a7ad032501e50982b283d8b997db8ae619
LinusOlofsson-maker <olofssonlinus97@gmail.com>
2022-10-13 12:30:24 +0200

    Made the fib and njit fib functions in python with a few prints to make sure they work.

--- MA4_Files/MA4_2.py
+++ MA4_Files/MA4_2.py
@@ -1,13 +1,45 @@
 #!/usr/bin/env python3
 
 from person import Person
+from numba import njit
+from time import perf_counter as pc
+
+
+def fib_classic(n):
+
+	if (n <= 1):
+		return n
+	else:
+		return fib_classic(n - 1) + fib_classic(n - 2)
+
+
+
+@njit
+def fib(n):
+
+	if (n <= 1):
+		return n
+	else:
+		return fib(n - 1) + fib(n - 2)
+
 
 def main():
+	n = 5
 	f = Person(5)
 	print(f.get())
 	f.set(7)
 	print(f.get())
 	print(f.fib())
+	start = pc()
+	fc = fib_classic(n)
+	end = pc()
+	print(f'For a pythonic fib value of {n} it is: {fc} and it took {end-start} seconds')
+	print(' ')
+	start = pc()
+	fib_njit = fib(n)
+	end = pc()
+	print(f'For a njit function for fib with value {n} it is: {fib_njit} and it took {end - start} seconds')
+
 
 if __name__ == '__main__':
 	main()
