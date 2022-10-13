ba0b2202fea6835a7c1626d00e51ff3d5ad6d5c2
LinusOlofsson-maker <olofssonlinus97@gmail.com>
2022-10-13 12:35:32 +0200

    Made the fib and njit fib functions in python with a few prints to make sure they work. added two for loops

--- MA4_Files/MA4_2.py
+++ MA4_Files/MA4_2.py
@@ -24,19 +24,26 @@ def fib(n):
 
 
 def main():
-	n = 5
+	n = 20
+	k = 20
+	fc = []
+	fib_njit = []
 	f = Person(5)
 	print(f.get())
 	f.set(7)
 	print(f.get())
 	print(f.fib())
 	start = pc()
-	fc = fib_classic(n)
+	for i in range(10):
+		fc.append(fib_classic(n))
+		n += 1
 	end = pc()
 	print(f'For a pythonic fib value of {n} it is: {fc} and it took {end-start} seconds')
 	print(' ')
 	start = pc()
-	fib_njit = fib(n)
+	for i in range(10):
+		fib_njit.append(fib(k))
+		k += 1
 	end = pc()
 	print(f'For a njit function for fib with value {n} it is: {fib_njit} and it took {end - start} seconds')
 
